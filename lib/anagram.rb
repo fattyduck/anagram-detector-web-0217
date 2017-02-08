978051bc615c041d8c0b4f54fe6408a2706925e4
dison  ruan <disonruan@disons-MacBook-Air.local>
2017-02-08 17:58:07 -0500

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
 # Your code goes here!
+class Anagram
+
+  attr_accessor :detector
+
+  def initialize(word)
+    @detector = word
+  end
+
+  def match(word_array)
+    word_array.select{|word| word.chars.sort.join == @detector.chars.sort.join }
+  end
+
+end
